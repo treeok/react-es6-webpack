0a0ba6dad1ce4a96b954cbb41e21f396b3e8dccf
treeok <wjc857398594@126.com>
2015-08-12 11:16:38 +0800

    alert && tabs

--- src/components/alert/index.js
+++ src/components/alert/index.js
@@ -4,6 +4,18 @@ import $ from 'jquery';
 import Template from './template.jsx';
 
 const Alert = React.createClass({
+    getInitialState() {
+        return {windowWidth: window.innerWidth, windowHeight: window.innerHeight ,scrollTop: document.body.scrollTop};
+    },
+    handleResize() {
+        this.setState({windowWidth: window.innerWidth,windowHeight: window.innerHeight ,scrollTop: document.body.scrollTop});
+    },
+    componentDidMount() {
+        window.addEventListener('resize' ,this.handleResize);
+    },
+    componentWillMount() {
+        window.removeEventListener('resize' ,this.handleResize);
+    },
     handleClick() {
         $(this.getDOMNode()).remove();
     },
